7b0d454e8052a5c75e060cbe35057b6e148cbfb0
dajung <kwonkkan@naver.com>
2024-08-08 17:54:50 +0900

    [level 0] Title: 짝수의 합, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = 0;
+    
+    for (var i = 0; i <= n; i++){
+        if(i % 2 === 0){
+            answer = answer + i;
+        }
+    }
+    return answer;
+}
