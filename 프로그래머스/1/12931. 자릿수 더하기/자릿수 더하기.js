686693208d4c9283f69b99f4bb96c337aa1f7ef4
dajung <kwonkkan@naver.com>
2024-08-17 00:19:46 +0900

    [level 1] Title: 자릿수 더하기, Time: 3.63 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -2,11 +2,12 @@ function solution(n)
 {
     var answer = 0;
 
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    
     while(n!==0){
         answer = answer + (n % 10);
         n = Math.floor(n / 10);
     }
-    
     console.log(answer);
 
     return answer;
