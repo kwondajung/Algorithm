a218b7536685b68a5a7cdce58d01f65bf41695e3
dajung <kwonkkan@naver.com>
2024-08-26 09:38:41 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.32 ms, Memory: 43.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,22 @@
+function solution(arr) {
+    var answer = [];
+    
+    const min = Math.min(...arr);
+    answer = min;
+    
+    const newArr = arr.filter(function(num){
+        if(num !== min){
+            return true;
+        } else {
+            return false;
+        }
+    });
+    
+    if(newArr.length > 0){
+        answer = newArr;
+    }else{
+        answer = [-1];
+    }
+    
+    return answer;
+}
