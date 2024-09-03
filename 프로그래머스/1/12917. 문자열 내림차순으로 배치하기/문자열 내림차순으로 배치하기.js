f261389b7d14026e1979a58acac62d42619829c2
다정 <kwonkkan@naver.com>
2024-09-03 09:50:38 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,17 +1,7 @@
 function solution(s) {
     var answer = '';
     
-    arr = [...s];
-    arr.sort(function compare(a, b){
-        if(a > b) {
-            return -1;
-        } 
-        if (a < b) {
-            return 1;
-        }
-        return 0;
-        
-    })
-    answer = arr.join("");
+    answer = [...s].sort().reverse().join("");
+    
     return answer;
 }
