378658d6f2518a74d29256c9bbf6c2338c1c5a84
다정 <kwonkkan@naver.com>
2024-09-30 09:40:45 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -3,10 +3,8 @@ function solution(n) {
     
     // 3진수로 변환
     const ternary = n.toString(3);
-    console.log(ternary);
     
     const reverseTernary = ternary.split('').reverse().join('');
-    console.log(reverseTernary);
     
     // 3진수를 10진수로 변환
     const decimal = parseInt(reverseTernary,3);
